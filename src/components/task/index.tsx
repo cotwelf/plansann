b8ba11319105f42305ecc0f7d60424d14fe4dcdd
cotwelf <812811926@qq.com>
2021-10-09 19:09:35 +0800

    update

--- src/components/task/index.tsx
+++ src/components/task/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import classNames from 'classnames'
 import { toggleModal } from '../../utils/base'
 import './style.scss'
 
 export const TaskItem: React.FC<any> = ({ status, name, per, unit }) => {
+  const [done, setDone] = useState()
+  const onDoneChange = (e: any) => {
+    if (typeof e.target.value === 'number' && e.target.value < 999) {
+      setDone(e.target.value)
+    } else {
+      setDone(0)
+    }
+  }
   const doTask = () => {
     toggleModal({
       title: 'dddd',
-      content: <div>test</div>
+      content: (
+        <React.Fragment>
+          本次完成
+          <input value={done} onChange={(e) => onDoneChange(e)}/>
+        </React.Fragment>
+      )
     })
   }
   return (
