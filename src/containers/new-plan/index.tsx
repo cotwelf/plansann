168ae099208c1b0974478c48cbb55facd2ede3ec
cotwelf <812811926@qq.com>
2022-02-04 12:04:40 +0800

    update

--- src/containers/new-plan/index.tsx
+++ src/containers/new-plan/index.tsx
@@ -13,7 +13,11 @@ const mapDispatchToProps = (dispatch: any) => bindActionCreators({
 
 const TNewPlan: React.FC = ({ toggleModal }: any) => {
   const [weekly, setWeekly] = useState(WEEKLY)
-  const [planData, setPlanData] = useState(null)
+  const [startTime, setStartTime] = useState('2022-02-03')
+  const [planData, setPlanData] = useState({
+    name: '背单词',
+    unit: '个'
+  })
   const onWeekClick = (item: any) => {
     const newItem = {
       ...item,
@@ -30,16 +34,31 @@ const TNewPlan: React.FC = ({ toggleModal }: any) => {
   const onClose = () => {
     toggleModal({},false)
   }
+  const onStartTimeChange = (e: any) => {
+    console.log(e.target.value)
+  }
+  const onEndTimeChange = (e: any) => {
+    console.log(e.target.value)
+  }
+  const onNameChange = (e: any) => {
+
+  }
+  const onTotalChange = (e: any) => {
+
+  }
+  const onUnitChange = (e: any) => {
+
+  }
   return (
     <Fragment>
       <div className='new-plan'>
       <span>我计划</span>
-      <input placeholder="2022-12-30" type="date" />
+      <input placeholder={startTime} type="date" onChange={onStartTimeChange} />
       至
-      <input placeholder="2022-12-30" type="date" />
-      <input placeholder="背单词" />
-      <input placeholder="300" type='number' />
-      <input placeholder="背单词" />，
+      <input placeholder="2022-12-30" type="date" onChange={onEndTimeChange} />
+      <input placeholder={planData.name} onChange={onNameChange} />
+      <input placeholder="300" onChange={onTotalChange} type='number' />
+      <input placeholder={planData.unit} onChange={onUnitChange} />，
       每周
       {weekly.map(item => (
         <span
@@ -48,6 +67,17 @@ const TNewPlan: React.FC = ({ toggleModal }: any) => {
           onClick={() => onWeekClick(item)}
         >{item.name}</span>
       ))}
+      执行。
+      <p>
+        当有多个计划同时进行的时候，我
+        <select>
+          <option value='test'>test</option>
+          <option value='test'>test</option>
+          <option value='test'>test</option>
+        </select>
+        希望优先完成这个计划。因此，我承诺在计划日内：
+      </p>
+      <p>每天背单词 30 个</p>
     </div>
     <div className="footer">
       <div className="btn cancel" onClick={onClose}>我再想想</div>
